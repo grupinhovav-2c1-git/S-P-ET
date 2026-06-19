895452ae7ac07ccb8359945f443c4a39dc1c313e
grupinhovav-2c1-git <grupinhovav@gmail.com>
2026-06-19 08:07:19 -0300

    Add files via upload

---
+++ Sindicato-De-Pesca-main/sindicato_pesca/includes/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <link rel="stylesheet" href="css/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sindicato De Pesca</title>
+</head>
+<body>
+    <header>
+        <h1></h1>
+
+    </header>
+    <main>
---
+++ Sindicato-De-Pesca-main/sindicato_pesca/index.php
@@ -0,0 +1,7 @@
+<?php
+require_once 'includes/header.php';
+?>
+
+<h2>Estoque da Farmácia</h2>
+
+<?php require_once 'includes/footer.php'; ?>
